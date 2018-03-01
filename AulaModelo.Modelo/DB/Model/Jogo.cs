299f0178401f1a833d5e73147828ff9ab41c7596
aluno <aluno@labivj.info>
2018-03-01 20:01:34 -0300

    First commit

---
+++ AulaModelo.Modelo/DB/Model/Jogo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaModelo.Modelo.DB.Model
+{
+    public class Jogo
+    {
+        public static List<Jogo> Jogos = new List<Jogo>() {
+            new Jogo {
+                Id = Guid.NewGuid(),
+                Titulo = "Chrono Trigger",
+                DtLancamento = new DateTime(),
+                Genero = "RPG",
+                Studio = "Square Enix"
+            },
+            new Jogo {
+                Id = Guid.NewGuid(),
+                Titulo = "Super Metroid",
+                DtLancamento = new DateTime(),
+                Genero = "Ação/Aventura",
+                Studio = "Nintendo"
+            },
+            new Jogo {
+                Id = Guid.NewGuid(),
+                Titulo = "Donkey Kong",
+                DtLancamento = new DateTime(),
+                Genero = "Aventura",
+                Studio = "Nintendo"
+            },
+            new Jogo {
+                Id = Guid.NewGuid(),
+                Titulo = "Zelda Breath Of The Wild",
+                DtLancamento = new DateTime(),
+                Genero = "Ação/Aventura/RPG",
+                Studio = "Nintendo"
+            },
+        };
+
+        public Guid Id { get; set; }
+        public String Titulo { get; set; }
+        public DateTime DtLancamento { get; set; }
+        public String Genero { get; set; }
+        public String Studio { get; set; }
+
+
+    }
+}
---
+++ AulaModelo/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using AulaModelo.Modelo.DB.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AulaModelo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(Jogo.Jogos);
+        }
+
+        public ActionResult InserirJogo()
+        {
+            return View("AlterarJogo", new Jogo());
+        }
+
+        public ActionResult AlterarJogo(Guid id)
+        {
+            var jogo = Jogo.Jogos.FirstOrDefault(f => f.Id == id);
+
+            if (jogo != null)
+            {
+                return View("AlterarJogo", jogo);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult GravarJogo(Jogo jogo)
+        {
+            if(jogo.Id.ToString() == "00000000-0000-0000-0000-000000000000")
+            {
+                jogo.Id = new Guid();
+                Jogo.Jogos.Add(jogo);
+            }
+            else
+            {
+                var p = Jogo.Jogos.FirstOrDefault(f => f.Id == jogo.Id);
+                p.Titulo = jogo.Titulo;
+                p.DtLancamento = jogo.DtLancamento;
+                p.Genero = jogo.Genero;
+                p.Studio = jogo.Studio;
+            }
+                        
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ApagarJogo(Guid id)
+        {
+            Jogo p = null;
+
+            foreach (var jogo in Jogo.Jogos)
+            {
+                if(jogo.Id == id)
+                {
+                    p = jogo;
+                    break;
+                }
+            }
+
+            if(p != null)
+            {
+                Jogo.Jogos.Remove(p);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DetalharJogo(Guid id)
+        {
+            var jogo = Jogo.Jogos.FirstOrDefault(f => f.Id == id);
+
+            if (jogo != null)
+            {
+                return View("DetalharJogo", jogo);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+
+}
