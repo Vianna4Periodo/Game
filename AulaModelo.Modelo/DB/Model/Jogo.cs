ba3915ce16a38d23c70aaed38016c4a3a4cfb936
aluno <aluno@labivj.info>
2018-03-01 21:11:10 -0300

    alteracoes

--- AulaModelo.Modelo/DB/Model/Jogo.cs
+++ AulaModelo.Modelo/DB/Model/Jogo.cs
@@ -11,31 +11,52 @@ namespace AulaModelo.Modelo.DB.Model
         public static List<Jogo> Jogos = new List<Jogo>() {
             new Jogo {
                 Id = Guid.NewGuid(),
-                Titulo = "Chrono Trigger",
+                Titulo = "Jogo 1",
                 DtLancamento = new DateTime(),
-                Genero = "RPG",
-                Studio = "Square Enix"
+                Genero = "Genero 1",
+                Estudio = "Estudio 1"
             },
             new Jogo {
                 Id = Guid.NewGuid(),
-                Titulo = "Super Metroid",
+                Titulo = "Jogo 2",
                 DtLancamento = new DateTime(),
-                Genero = "Ação/Aventura",
-                Studio = "Nintendo"
+                Genero = "Genero 2",
+                Estudio = "Estudio 2"
             },
             new Jogo {
                 Id = Guid.NewGuid(),
-                Titulo = "Donkey Kong",
+                Titulo = "Jogo 3",
                 DtLancamento = new DateTime(),
-                Genero = "Aventura",
-                Studio = "Nintendo"
+                Genero = "Genero 3",
+                Estudio = "Estudio 3"
             },
             new Jogo {
                 Id = Guid.NewGuid(),
-                Titulo = "Zelda Breath Of The Wild",
+                Titulo = "Jogo 4",
                 DtLancamento = new DateTime(),
-                Genero = "Ação/Aventura/RPG",
-                Studio = "Nintendo"
+                Genero = "Genero 4",
+                Estudio = "Estudio 4"
+            },
+            new Jogo {
+                Id = Guid.NewGuid(),
+                Titulo = "Jogo 5",
+                DtLancamento = new DateTime(),
+                Genero = "Genero 5",
+                Estudio = "Estudio 5"
+            },
+            new Jogo {
+                Id = Guid.NewGuid(),
+                Titulo = "Jogo 6",
+                DtLancamento = new DateTime(),
+                Genero = "Genero 6",
+                Estudio = "Estudio 6"
+            },
+            new Jogo {
+                Id = Guid.NewGuid(),
+                Titulo = "Jogo 7",
+                DtLancamento = new DateTime(),
+                Genero = "Genero 7",
+                Estudio = "Estudio 7"
             },
         };
 
@@ -43,7 +64,7 @@ namespace AulaModelo.Modelo.DB.Model
         public String Titulo { get; set; }
         public DateTime DtLancamento { get; set; }
         public String Genero { get; set; }
-        public String Studio { get; set; }
+        public String Estudio { get; set; }
 
 
     }
